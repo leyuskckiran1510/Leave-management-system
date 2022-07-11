5f3adff6c6ea00e2f5a133a3d688ec3a8cb81742
leyuskckiran1510 <kdhakal1510@gmail.com>
2022-07-11 14:02:18 +0545

    1

---
+++ LMS.c
@@ -0,0 +1,6 @@
+//Author:Kiran Raj Dhakal
+//Date:2022/7/11
+//Description:A program to implemet Leave Management System in C 
+//            with file handling and error handling
+//            The program is used to add,delete,modify and display the leaves of the employee
+//            with admin,manager and employee login
