96f3e2f1dffcb433b813eedfc4727d6978d8dadd
leyuskckiran1510 <kdhakal1510@gmail.com>
2022-07-12 05:31:48 +0545

    Some added Gui looking outputs/stdout

--- LMS.c
+++ LMS.c
@@ -7,6 +7,23 @@ exit ${?}
 //can be ingnored if you want to compile and execute separately with gcc
 
 
+//###############################################################################//
+/*  ::           ::::    ::::     :::::::  (Leave Management System)
+    ::           :: ::  :: ::    :: 
+    ::           ::  ::::  ::    ::
+    ::           ::        ::     :::::: 
+    ::           ::        ::           ::
+    ::           ::        ::           ::
+    ::::::::::   ::        ::     :::::::
+
+                            BY- Kiran Raj Dhakal
+*/
+///////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
 //Author:Kiran Raj Dhakal
 //Date:2022/7/11
 //Description:A program to implemet Leave Management System in C 
@@ -20,22 +37,27 @@ exit ${?}
 #include<time.h>
 
 
+//ansice code for screen clearing and moving cursor to the top left corner
+//referenced from  https://en.wikipedia.org/wiki/ANSI_escape_code
+#define cls printf("\033[2J\033[1;1H");
+
+
 struct employe{
-    char id[20];
-    char name[20];
-    char dob[20];
-    char phone[20];
-    char email[20];
-    char password[20];
-    char type[20];
+    char id[50];
+    char name[50];
+    char dob[50];
+    char phone[50];
+    char email[50];
+    char password[50];
+    char type[50];
     int leaves;
-    char doj[20];
+    char doj[50];
 };
 
 //struct leaves is used to store the leaves of the employee with 
 //the employee id and date is used to store the date of the leaves
 struct leaves{
-    char id[20];
+    char id[50];
     long int date;
     
 };
@@ -53,8 +75,6 @@ int login(char *username,char *password){
         exit(0);
     }
     while(fscanf(fp,"%s %s %s %s %s %s %s %d %s",e.id,e.name,e.dob,e.phone,e.email,e.password,e.type,&e.leaves,e.doj)!=EOF){
-        printf("%s %s %s %s\n",e.name,e.password,username,password);
-        printf("%d\n",strcmp(e.name,username));
         if(strcmp(e.name,username)==0 && strcmp(e.password,password)==0){
             fclose(fp);
             printf("Login Successful\n");
@@ -214,8 +234,66 @@ void gen_id(struct employe *b){
     strcpy(b->id,str);
     }
 
+void login_screen(char *n,char *p){
+    //deginse of a login in screen with the name and password as parameters
+    //using ansi escape sequence
+    cls;
+    printf("\033[10;50H\033[38;2;150;15;245mLogin Screen\033[0m\n");
+    printf("\033[13;40H\033[38;2;40;205;205mEnter name:\033[0m\n");
+    printf("\033[15;40H\033[38;2;205;40;205mEnter password:\033[0m\n");
+    printf("\033[13;56H\033[38;2;50;255;255m");
+    scanf("%s",n);
+    fflush(stdin);
+    printf("\033[15;56H\033[38;2;255;50;255m");
+    scanf("%s",p);
+    printf("\033[0m\n");
+    cls;
+
+}
+//employee adding Screen
+void employee_add_Screen(){
+    struct employe e;
+    char *date;
+    time_t t = time(NULL);
+    date = ctime(&t);
+    strcpy(e.doj,date);
+    e.leaves=5;
+    cls;
+    printf("\033[9;50H\033[38;2;150;15;245mEmployee Add Screen\033[0m\n");
+    printf("\033[12;40H\033[38;2;40;205;205mEnter NAME:\033[0m\n");
+    printf("\033[13;40H\033[38;2;205;40;205mEnter DOB:\033[0m\n");
+    printf("\033[14;40H\033[38;2;40;205;205mEnter PHONE:\033[0m\n");
+    printf("\033[15;40H\033[38;2;205;40;205mEnter EMAIL:\033[0m\n");
+    printf("\033[16;40H\033[38;2;205;40;205mEnter PASSWORD:\033[0m\n");
+    printf("\033[17;40H\033[38;2;40;205;205mEnter TYPE:\033[0m\n");
+    printf("\033[12;56H\033[38;2;50;255;255m");
+    scanf("%s",e.name);
+    printf("\033[13;56H\033[38;2;50;255;255m");
+    scanf("%s",e.dob);
+    printf("\033[14;56H\033[38;2;50;255;255m");
+    scanf("%s",e.phone);
+    printf("\033[15;56H\033[38;2;50;255;255m");
+    scanf("%s",e.email);
+    printf("\033[16;56H\033[38;2;50;255;255m");
+    scanf("%s",e.password);
+    printf("\033[17;56H\033[38;2;50;255;255m");
+    scanf("%s",e.type);
+    strcpy(e.id,"Blank");
+    gen_id(&e);
+    printf("%s",e.id);
+    cls;
+
+    
+   
+
+
+}
+
 
 int main(){
+    int status;
+    //char name[50];
+    //char password[50];
     //statically declare admin employee details 
     //struct employe admin={"admin!","admin","2022/1/1","+9779800000000","admin@admin.com","696969","admin",10,"2022"};
     //gen_id(&admin);
@@ -224,5 +302,9 @@ int main(){
     //delete_employee(e);
     //modify_employee(e);
     //display_employee(e);
+    //login_screen(name,password);
+    //status=login(name,password);
+    //printf("%d\n",status);
+    employee_add_Screen();
     return 0;
 }
